10b1c640e6a3ee7cda2cf61010e29f0425e96716
jinwook song <wlsdnr129@naver.com>
2024-01-21 20:45:08 +0900

    stream response

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
@@ -13,7 +14,28 @@ import streamlit as st
 
 st.set_page_config(page_title="DocumentGPT", page_icon="📃")
 
-llm = ChatOpenAI(temperature=0.1)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token: str, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ],
+)
 
 
 @st.cache_data(show_spinner=True)
@@ -46,13 +68,15 @@ def embed_file(file):
     return retriever
 
 
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        st.session_state["messages"].append(
-            {"message": message, "role": role},
-        )
+        save_message(message, role)
 
 
 def paint_history():
@@ -118,8 +142,8 @@ if file:
             | prompt
             | llm
         )
-        response = chain.invoke(message)
-        send_message(response.content, "ai")
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
 
 
 else:
